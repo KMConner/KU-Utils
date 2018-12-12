128a38c23b65e194b25343f279709cce051f87f2
KMConner <KMConner@users.noreply.github.com>
2018-12-12 11:27:33 +0900

    Added Solution and project

---
+++ KuUtils/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KuUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
